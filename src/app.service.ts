a1e06991be55a0116a681faf5739c3d07a3c1ad2
xxM4fuxx <121265515+xxM4fuxx@users.noreply.github.com>
2023-09-10 16:15:56 -0600

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Gabo my old friend!';
+    return 'Hello World from mafu backend!';
   }
 }
