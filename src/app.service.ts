d97dcf7025bd86c5dca83e114cb3f84de6cacc51
Gabriel Gonzalez <mafu.77.gg@gmail.com>
2024-05-19 12:19:07 -0600

    Se creo la carpeta distx2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from MAFU HOME!';
+    return 'Hello World from MAFU HOME test 1!';
   }
 }
