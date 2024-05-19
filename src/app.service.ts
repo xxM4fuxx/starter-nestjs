ffd562b85e5e14cb0a001462c784339ef65908e8
Gabriel Gonzalez <mafu.77.gg@gmail.com>
2024-05-19 12:44:13 -0600

    Se regreso al estado normal sin dist

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hola desde el dev';
+    return 'Hola de nuevo desde ciclic';
   }
 }
